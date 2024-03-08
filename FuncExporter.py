336ab7d98476e259ccfe31c995e0a9547d5e887b
ᴋᴇʏ <24655118+gh0stkey@users.noreply.github.com>
2024-03-08 15:16:36 +0800

    Update and rename export_func_code.py to FuncExporter.py

--- FuncExporter.py
+++ FuncExporter.py
@@ -1,20 +1,22 @@
 # -*- coding:utf-8 -*-
-import os
+
 from idaapi import plugin_t
 from idaapi import PLUGIN_PROC
 from idaapi import PLUGIN_OK
+from PyQt5 import QtWidgets
+
+import os
 import ida_nalt
 import idaapi
 import idautils
 import idc
 import time
 
-
-# 获取SO文件名和路径
-def getSoPathAndName():
+# 获取当前反编译的文件名
+def getSoName():
     fullpath = ida_nalt.get_input_file_path()
-    filepath,filename = os.path.split(fullpath)
-    return filepath,filename
+    filepath, filename = os.path.split(fullpath)
+    return filename
 
 # 获取代码段的范围
 def getSegAddr():
@@ -33,27 +35,40 @@ def getSegAddr():
 
     return min(textStart), max(textEnd)
 
+class myForm(QtWidgets.QDialog):
+    def __init__(self, parent=None):
+        super(myForm, self).__init__(parent)
+
+    def selectDir(self):
+        dir_path = QtWidgets.QFileDialog.getExistingDirectory(self, 'Select a directory')
+        return dir_path
 
 class traceNatives(plugin_t):
     flags = PLUGIN_PROC
-    comment = "export_func_code"
+    comment = "FuncExporter"
     help = ""
-    wanted_name = "export_func_code"
+    wanted_name = "FuncExport"
     wanted_hotkey = ""
 
     def init(self):
-        print("export_func_code(v0.1) plugin has been loaded.")
+        print("FuncExport(v0.1) plugin has been loaded.")
+        print("Original author: https://github.com/jitcor/export_func_code")
         return PLUGIN_OK
-
+    
     def run(self, arg):
-        # 查找需要的函数
         ea, ed = getSegAddr()
-        so_path, so_name = getSoPathAndName()
-        script_name = so_name.split(".")[0] + "_" + str(int(time.time())) +".txt"
-        save_path = os.path.join(so_path, script_name)
-        print(f"导出路径：{save_path}")
-        F=open(save_path, "w+", encoding="utf-8")
-        F.write("\n#####################################\n")
+        so_name  = getSoName()
+        form = myForm()
+        dir_path = form.selectDir()
+        # 使用用户选择的目录
+        save_path = f"{dir_path}/{so_name.split('.')[0]}"
+
+        if not os.path.isdir(save_path):
+            save_path = f"{save_path}_export"
+
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
+
         for func in idautils.Functions(ea, ed):
             try:
                 functionName = str(idaapi.ida_funcs.get_func_name(func))
@@ -62,18 +77,16 @@ class traceNatives(plugin_t):
                     arm_or_thumb = idc.get_sreg(func, "T")
                     if arm_or_thumb:
                         func += 1
-                    code=str(idaapi.decompile(func))+"\n#####################################\n"
-                    print(code)
-                    F.write(code)
-                    F.flush()
+                    code = str(idaapi.decompile(func))
+                    save_func_filename = f"{save_path}/{functionName}.c"
+                    with open(save_func_filename, "w+", encoding="utf-8") as f:
+                        f.write(code)
+                    print(f"Successfully saved in file: {save_func_filename}")
             except Exception as e:
                 print(e)
-        print(f"导出完成：{save_path}")
-        F.close()
 
     def term(self):
         pass
-
 
 def PLUGIN_ENTRY():
     return traceNatives()
